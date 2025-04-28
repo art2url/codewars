d3769bfe8235ffb06d04603b982fa7c704b6120d
Artem Turlenko <artem.turlenko@gmail.com>
2025-04-28 07:23:11 +0200

    5-kyu – Calculating with Functions.js (refactor)

--- CodeWars-5-kyu-Solutions/Calculating with Functions.js
+++ CodeWars-5-kyu-Solutions/Calculating with Functions.js
@@ -17,57 +17,67 @@ Requirements:
 eight(dividedBy(three()));
 */
 
-function zero(fn) {
-  return fn ? fn(0) : 0;
-}
-function one(fn) {
-  return fn ? fn(1) : 1;
-}
-function two(fn) {
-  return fn ? fn(2) : 2;
-}
-function three(fn) {
-  return fn ? fn(3) : 3;
-}
-function four(fn) {
-  return fn ? fn(4) : 4;
-}
-function five(fn) {
-  return fn ? fn(5) : 5;
-}
-function six(fn) {
-  return fn ? fn(6) : 6;
-}
-function seven(fn) {
-  return fn ? fn(7) : 7;
-}
-function eight(fn) {
-  return fn ? fn(8) : 8;
-}
-function nine(fn) {
-  return fn ? fn(9) : 9;
-}
+// function zero(fn) {
+//   return fn ? fn(0) : 0;
+// }
+// function one(fn) {
+//   return fn ? fn(1) : 1;
+// }
+// function two(fn) {
+//   return fn ? fn(2) : 2;
+// }
+// function three(fn) {
+//   return fn ? fn(3) : 3;
+// }
+// function four(fn) {
+//   return fn ? fn(4) : 4;
+// }
+// function five(fn) {
+//   return fn ? fn(5) : 5;
+// }
+// function six(fn) {
+//   return fn ? fn(6) : 6;
+// }
+// function seven(fn) {
+//   return fn ? fn(7) : 7;
+// }
+// function eight(fn) {
+//   return fn ? fn(8) : 8;
+// }
+// function nine(fn) {
+//   return fn ? fn(9) : 9;
+// }
 
-function plus(right) {
-  return function (left) {
-    return left + right;
-  };
-}
-function minus(right) {
-  return function (left) {
-    return left - right;
-  };
-}
-function times(right) {
-  return function (left) {
-    return left * right;
-  };
-}
-function dividedBy(right) {
-  return function (left) {
-    return Math.floor(left / right);
-  };
-}
+// function plus(right) {
+//   return function (left) {
+//     return left + right;
+//   };
+// }
+// function minus(right) {
+//   return function (left) {
+//     return left - right;
+//   };
+// }
+// function times(right) {
+//   return function (left) {
+//     return left * right;
+//   };
+// }
+// function dividedBy(right) {
+//   return function (left) {
+//     return Math.floor(left / right);
+//   };
+// }
+
+const [zero, one, two, three, four, five, six, seven, eight, nine] = Array.from(
+  {length: 10},
+  (_, n) => (fn) => fn ? fn(n) : n
+);
+
+const plus = (y) => (x) => x + y;
+const minus = (y) => (x) => x - y;
+const times = (y) => (x) => x * y;
+const dividedBy = (y) => (x) => Math.floor(x / y);
 
 console.log(seven(times(five()))); // 35
 console.log(four(plus(nine()))); // 13
